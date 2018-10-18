f230f726b92dea24e6922025edc884a0388e45d7
dogJions <hasee@windows10.microdone.cn>
2018-10-18 10:34:15 +0800

    111

--- src/main/java/com/fym/controller/BaseController.java
+++ src/main/java/com/fym/controller/BaseController.java
@@ -1,9 +1,9 @@
 package com.fym.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+//1111
 public class BaseController<T> {
-
+   
 	@Autowired
 	protected T baseService;
 }
